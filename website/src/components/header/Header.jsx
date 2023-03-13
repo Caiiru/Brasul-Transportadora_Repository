d83a2a5ec90e1e3ee1b562cd871f9eeaf287749d
Caiiru <54562856+Caiiru@users.noreply.github.com>
2023-03-13 17:59:25 -0300

    Slidershow commit (unfinished)

--- website/src/components/header/Header.jsx
+++ website/src/components/header/Header.jsx
@@ -1,24 +1,60 @@
 import React from 'react'
 import './header.css'
-
+import 'react-slideshow-image/dist/styles.css';
+import { Slide } from 'react-slideshow-image';
 import { header1, header2 } from './imports.js'
+
 const Header = () => {
-  let slideIndex=1;
+  let slideIndex = 1;
   showSlides(slideIndex);
 
-  function plusSlides(n){
-    showSlides(slideIndex+=n)
+  function plusSlides(n) {
+    showSlides(slideIndex += n)
 
   }
-  function currentSlide(n){
+  function currentSlide(n) {
     showSlides(slideIndex = n);
   }
 
-  function showSlides(n){
+  function showSlides(n) {
     let i;
   }
+
+  const slideImages = [
+    "https://img.freepik.com/free-photo/trucks-highway-mountain-sunset_181624-61739.jpg?w=1380&t=st=1678738139~exp=1678738739~hmac=cb87f2ffd50236c5b565e8844375c92fb409b9dc41f122024a9ec47cba7966ee",
+    "https://img.freepik.com/free-photo/truck-vehicle-with-trailers-background_342744-1297.jpg?w=1380&t=st=1678739100~exp=1678739700~hmac=ae514faee7164a1cd404d989173a9a5f3bc1c88d7d859859f9a4ab98e8b54020",
+
+  ];
+  const divStyle = {
+  }
+
+  const properties = {
+    duration: 5000,
+    backgroundSize:'contain',
+    autoplay: false,
+    transitionDuration: 500,
+    arrows: true,
+    infinite: true,
+    easing: 'ease',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundSize: 'cover',
+    height: '720px'
+  };
   return (
+
+
     <div className='brasul__site-header'>
+      <div className='slide-container'>
+        <Slide  {...properties}>
+          {slideImages.map((each, index) => (
+            <div className='each-slide' key={index}>
+              <img className='lazy' src={each} alt='sample' />
+            </div>
+          ))}
+        </Slide>
+      </div>
       <div className='brasul__site-header-slidershow'>
         <div className='brasul__site-header-slidershow_fade'>
           <div className='brasul__site-header-slidershow_numbertext'> 1 / 2</div>
@@ -29,14 +65,6 @@ const Header = () => {
           <img src={header2} className='Image-Header' alt='header-truck2' />
         </div>
 
-        <a className='prev' onClick='plusSlides(-1)'> &#10094;</a>
-        <a className='next' onClick='plusSlides(1)'> &#10095;</a>
-
-      </div>
-      <div className='dots'>
-        <span class="dot" onclick="currentSlide(1)"></span>
-        <span class="dot" onclick="currentSlide(2)"></span>
-        <span class="dot" onclick="currentSlide(3)"></span>
       </div>
       <div className='brasul__site-header-orcamento'>
         <button type='button'>Solicitar Orçamento</button>
